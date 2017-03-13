41ce58207af2fb980db4a1dad54e65bcbcedba77
wangying <867504146@qq.com>
2017-03-13 23:14:36 +0800

    提交展示照片

---
+++ MusicPlayer/HttpGetResouce.cpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////
+// 从API中获取需要的数据
+
+#include "stdafx.h"
+#include "HttpGetResouce.h"
+
+CHttpGetResouce::CHttpGetResouce()
+{
+
+}
+
+CHttpGetResouce::~CHttpGetResouce()
+{
+
+}
+
+BOOL CHttpGetResouce::GetLyric(SStringW strName)
+{
+	
+	return TRUE;
+}
---
+++ MusicPlayer/HttpGetResouce.h
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////
+// 从API中获取需要的数据
+
+#pragma once
+
+class CHttpGetResouce
+{
+public:
+	CHttpGetResouce();
+	virtual ~CHttpGetResouce();
+
+	BOOL GetLyric(SStringW strName);
+
+protected:
+
+};
